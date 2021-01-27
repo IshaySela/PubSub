1d39863e00a1d26700d25a2ec7be50ecdff9e879
IshaySela <ishaisela@gmail.com>
2021-01-27 15:34:19 +0200

    Documented Publisher.hpp

--- source/headers/Publisher.hpp
+++ source/headers/Publisher.hpp
@@ -17,6 +17,10 @@ namespace PubSub
     using SubscribersMap = std::unordered_map<std::string, std::unique_ptr<Subscriber>>;
     
     class TlsSocketServer;
+
+    /**
+     * @brief The class publisher listens for data from the client and emits it to all of the subscribers. 
+    */
     class Publisher
     {
     public:
@@ -47,18 +51,19 @@ namespace PubSub
         std::future<void> emit(const char *buffer, size_t size);
 
     private:
-        std::string id;
-        SubscribersMap subscribers;
-        std::shared_future<void> workerFtr;
-        ClientBIO client;
-        std::atomic<bool> workerDone;
-        TlsSocketServer& server;
+        std::string id; ///< The id of the publisher, as stored in the publishers map of the server object.
+        SubscribersMap subscribers; ///< Map of all of the subscribers.
+        std::shared_future<void> workerFtr; ///< The future of the worker thread. This makes sure that when the publisher is removed, the worker thread will exit gracefully. 
+        ClientBIO client; ///< The socket of the client.
+        std::atomic<bool> workerDone; ///< TODO: Remove.
+        TlsSocketServer& server; ///< Reference to the server. Will be used to remove this publisher when the connection is dropped.
+        
         /**
          * @brief listen to the socket and emit all of the values to the clients.
         */
         void worker();
         /**
-         * @brief Check for any exception thrown by Publisher::worker, and close the publisher if the error is fatal.
+         * @brief Catch any exception thrown by Publisher::worker, and close the publisher if the error is fatal.
         */
         void workerWrapper();
     };
