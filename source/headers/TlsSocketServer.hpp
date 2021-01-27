b1ea9d74329bc571b3327070cf8d7d364526f00e
IshaySela <ishaisela@gmail.com>
2021-01-27 15:18:00 +0200

    Documented class TlsSocketServer in TlsSocketServer.hpp

--- source/headers/TlsSocketServer.hpp
+++ source/headers/TlsSocketServer.hpp
@@ -18,37 +18,53 @@ using PubSub::Util::ClientsFactoryBIO;
 namespace PubSub
 {
     class Publisher;
+    /**
+     * @brief Listen to incoming connections and sorts the clients to publishers and subscriber, according to the handshake the client sends.
+    */
     class TlsSocketServer
     {
     public:
         TlsSocketServer(int port);
         ~TlsSocketServer();
-        
 
+        /**
+         * @brief Accepts clients, recive the handshake and handling the client accordingly.
+        */
         void acceptClients();
         /**
          * @brief Add the string given to the disconnectedPublishsers list.
          * Note this function does not actually remove the publisher from the publishers map, but signals the cleaner thread to remove the publisher. 
-         * @param std::string The id of the publisher to remove.
+         * @param id The id of the publisher to remove.
         */
         void removePublisher(std::string id);
     private:
         int port;
-        ClientsFactoryBIO clientsFactory;
-        std::unordered_map<std::string, std::unique_ptr<Publisher>> publishers;
+        ClientsFactoryBIO clientsFactory; ///< The main BIO the clients will connect to.
+        std::unordered_map<std::string, std::unique_ptr<Publisher>> publishers; ///< A map of all of the publishers.
+
+        std::vector<std::string> disconnectedPublishsers; ///< When a publisher is done, its id will be pushed into this array, for the publisher cleaner to remove.
+        std::mutex disconnectedPublishsersLock; ///< The lock for the disconnectedPublishsers array
 
         /**
-         * When a publisher is done, its id will be pushed into this array, for the publisher cleaner to remove. 
+         * @brief Get the latest client and return it.
+         * @returns The latest client.
         */
-        std::vector<std::string> disconnectedPublishsers;
-        // The lock for the disconnectedPublishsers array.
-        std::mutex disconnectedPublishsersLock;
-
         ClientBIO acceptNewTcpConnection();
-        
+
+        /**
+         * @brief Create a publisher and add it to the publishers list. Send the handshake response back to the client.
+        */
         void handlePublisher(ClientBIO& socket);
+        /**
+         * @brief Create a subscriber and add it to requested publisher.
+         * @param socket The socket of the subscriber.
+         * @param handshake The handshake the client sent.
+        */
         void handleSubscriber(ClientBIO& socket, Protocol::Handshake& handshake);
 
+        /**
+         * @brief Iterate over the disconnected publishers list and remove the dead connections.
+        */
         void publishersCleaner();
     };
 }
