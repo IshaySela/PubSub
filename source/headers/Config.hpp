7eeb8b8307603ce311ee8191f4969daabe23fc10
IshaySela <ishaisela@gmail.com>
2021-02-14 14:38:06 +0200

    Added config class with deleted ctor

---
+++ source/headers/Config.hpp
@@ -0,0 +1,16 @@
+
+
+namespace PubSub
+{
+    class Config
+    {
+    public:
+        Config() = delete;
+        Config(Config& other) = delete;
+
+        ~Config();
+    private:
+    
+    };
+        
+}
